175c6db1137e234ac439c27bdf5ac6478dc45952
Sean Stacey <kumori@ryougi.com>
2022-06-27 09:57:12 -0400

    Semi-functional 1.4 deployment

--- kube/tmodloader1.4/docker-entrypoint.sh
+++ kube/tmodloader1.4/docker-entrypoint.sh
@@ -1,6 +1,9 @@
+#!/usr/bin/env bash
 cd "$(dirname "$0")"
 
 chmod a+x ./LaunchUtils/ScriptCaller.sh
-# forward our parent process id to the child in case ScriptCaller needs to kill the parent to break free of steam's process lifetime tracker (reaper)
 
-PPID=$PPID ./LaunchUtils/ScriptCaller.sh -config serverconfig.txt -server -tmlsavedirectory $DATA_DIR/data -modpath $DATA_DIR/mods -world $DATA_DIR/data/Worlds/WHAL.wld -autocreate 1 -worldname WHAL -seed WHALSeed -difficulty 1
+screen -S Terraria -L -Logfile ${DATA_DIR}/masterLog.0 -d -m ./LaunchUtils/ScriptCaller.sh -config serverconfig.txt -server -tmlsavedirectory $DATA_DIR/data -modpath $DATA_DIR/mods -world $DATA_DIR/data/Worlds/WHAL.wld -autocreate 1 -worldname WHAL -seed WHALSeed -difficulty 1 &
+
+sleep 2
+tail -f ${DATA_DIR}/masterLog.0
