80f9f29deb8c9f02bcc9dc6c25d2635127e512b0
Sean Stacey <kumori@ryougi.com>
2023-08-03 18:23:52 -0400

    Updated tmodloader to .28

--- docker/terraria/docker-entrypoint.sh
+++ docker/terraria/docker-entrypoint.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
+
 cd "$(dirname "$0")"
 
 chmod a+x ./LaunchUtils/ScriptCaller.sh
 
-screen -S Terraria -L -Logfile ${DATA_DIR}/masterLog.0 -d -m ./LaunchUtils/ScriptCaller.sh -config serverconfig.txt -server -tmlsavedirectory $DATA_DIR/data -modpath $DATA_DIR/mods -world $DATA_DIR/data/Worlds/WHAL.wld -autocreate 1 -worldname WHAL -seed WHALSeed -difficulty 1 &
+echo "Starting Terraria Server..."
+screen -S Terraria -L -Logfile ${DATA_DIR}/masterLog.0 -d -m ./LaunchUtils/ScriptCaller.sh -config serverconfig.txt -server -tmlsavedirectory $DATA_DIR/data -modpath $DATA_DIR/mods $@ &
 
 sleep 2
 tail -f ${DATA_DIR}/masterLog.0
