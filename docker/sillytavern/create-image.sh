2344b9613d8148deeea7dc3d09975f521838f7ba
Sean Stacey <kumori@ryougi.com>
2024-01-11 19:35:31 -0500

    All updates made in the past few weeks, mostly maintence

--- docker/sillytavern/create-image.sh
+++ docker/sillytavern/create-image.sh
@@ -1,12 +1,13 @@
 
 # Clone the repository
 git clone https://github.com/SillyTavern/SillyTavern.git --depth 1 --branch release --single-branch
-cd SillyTavern
+cp config.yaml SillyTavern/default/config.yaml
 
 # Build the image
-docker build -t registry.home.ryougi.ca/st .
+cd SillyTavern
+docker build -t registry.home.ryougi.ca/st . --no-cache
 docker push registry.home.ryougi.ca/st
 
 # Cleanup
 cd ..
-rm -rf SillyTavern
+# rm -rf SillyTavern
