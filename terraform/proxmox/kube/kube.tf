f1f771ce2ebab706e51ea8a249da214f45a7c923
Sean Stacey <kumori@ryougi.com>
2024-05-11 18:59:23 -0400

    Rebuilt kube nodes to use new image

--- terraform/proxmox/kube/kube.tf
+++ terraform/proxmox/kube/kube.tf
@@ -1,4 +1,3 @@
-
 resource "proxmox_vm_qemu" "kube-master-vm" {
   name = "kube-master-1"
   desc = "A test for using terraform and cloudinit"
@@ -37,9 +36,74 @@ resource "proxmox_vm_qemu" "kube-master-vm" {
 
   # Setup the disk
   disk {
-    size         = "32G"
+    size         = "64G"
     type         = "scsi"
-    storage      = "local-lvm"
+    storage      = "pve-lvm"
+    ssd          = 0
+    discard      = "on"
+  }
+
+  #VLAN ID
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+    tag    = 2
+  }
+
+  ciuser  = "kumori"
+  sshkeys = <<EOF
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINOwoZ0u3t1QuHtElHSKaw2P6kUYF9cLnUcGjcKXErqZ kumori@DESKTOP-3N1AKAV
+  EOF
+
+  timeouts {
+    create = "60m"
+    delete = "2h"
+  }
+
+}
+
+
+resource "proxmox_vm_qemu" "kube-master-vm-2" {
+  name = "kube-master-2"
+  desc = "A test for using terraform and cloudinit"
+
+  # Node name has to be the same name as within the cluster
+  # this might not include the FQDN
+  target_node = "pve"
+
+  # The destination resource pool for the new VM
+  # pool = "kube"
+
+  # provider fails if I try touch pools again
+  lifecycle {
+    ignore_changes = [
+      pool
+    ]
+  }
+
+  # The template name to clone this vm from
+  # This is currently setup manually as a template image
+  clone = "ubuntu-base"
+
+  # Activate QEMU agent for this VM
+  agent = 1
+
+  os_type = "cloud-init"
+  qemu_os = "l26"
+  cores   = 4 
+  sockets = 1
+  vcpus   = 0
+  cpu     = "host"
+  memory  = 4096
+  scsihw  = "virtio-scsi-pci"
+
+  ipconfig0 = "ip=192.168.16.23/20,gw=192.168.16.1"
+
+  # Setup the disk
+  disk {
+    size         = "64G"
+    type         = "scsi"
+    storage      = "pve-lvm"
     ssd          = 0
     discard      = "on"
   }
@@ -102,7 +166,7 @@ resource "proxmox_vm_qemu" "kube-vm" {
   disk {
     size         = "64G"
     type         = "scsi"
-    storage      = "local-lvm"
+    storage      = "pve-lvm"
     ssd          = 0
     discard      = "on"
   }
