b6eea4b34483556e89402c697b2aa717f10fd662
Sean Stacey <kumori@ryougi.com>
2024-10-19 01:37:33 -0400

    Moving to repo level possibly

--- docker/terraria/build.sh
+++ docker/terraria/build.sh
@@ -7,13 +7,13 @@ GH_TOKEN=$(echo $GH_PAT | base64)
 TAG=$(curl -sL https://api.github.com/repos/tModLoader/tModLoader/releases | jq -r "first | .tag_name")
 UPLOADED_TAGS=$(curl -H "Authorization: Bearer $GH_TOKEN" -s "https://ghcr.io/v2/${repo}/tags/list" | jq -r ".tags[]")
 
+echo "Repo: $repo"
 echo "Latest version of tModLoader server is $TAG"
 echo "Checking if tag $TAG exists on ghcr..."
 echo "Uploaded tags: $UPLOADED_TAGS"
 
 if echo $UPLOADED_TAGS | grep -q $TAG; then
   echo "Tag $TAG already exists on ghcr"
-  exit 1
 else
   echo "Tag $TAG does not exist on ghcr"
   echo "Building image..."
